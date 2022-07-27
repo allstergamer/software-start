acd8a21022868a731870f762280424a7e671c2f6
Allster <63209264+allstergamer@users.noreply.github.com>
2022-07-27 14:45:42 +0200

    Add files via upload

--- most used apps.py
+++ most used apps.py
@@ -5,6 +5,11 @@ import webbrowser
 import os
 
 
+
+
+def rgb_hack(rgb):
+    return "#%02x%02x%02x" % rgb
+
 #website definition i need
 def open_whatsapp():
     webbrowser.open_new("https://web.whatsapp.com/")
@@ -56,71 +61,71 @@ def open_azn():
 
 
 def work():
-    work_there.configure(bg='darkgreen')
+    work_there.configure(bg=rgb_hack((141, 192, 252)))
     #tkinter.messagebox.showinfo('work', 'now u are working!')
     #tkinter.messagebox.showwarning('work', 'now u are working!')
     #tkinter.messagebox.showerror('work', 'now u are working!')
-    root=tkinter.Tk()
-    
-    root.title('WORK')  
-    root.geometry('500x700')  
-    root.configure(bg='darkgreen')  
-    
+    worked=tkinter.Tk()
+    worked.title('WORK')  
+    worked.geometry('500x700')  
+    worked.configure(bg=rgb_hack((82, 161, 240)))  
   
     #icon for work
-    root.iconbitmap("work.ico")
+    worked.iconbitmap("work.ico")
 
 
     #part for apps i need on work  
 
-    whatsapp_web = Button(root, text="web.whatsapp.com", font=("helvetica", 14), width="40", bg='lightgreen', command=open_whatsapp)
+    whatsapp_web = Button(worked, text="web.whatsapp.com", font=("helvetica", 14), width="40", bg=rgb_hack((141, 192, 252)), command=open_whatsapp)
     whatsapp_web.pack(pady=3)
 
-    spotify = Button(root, text="spotify.com", font=("helvetica", 14), width="40", bg='lightgreen', command=open_spotify)
+    spotify = Button(worked, text="spotify.com", font=("helvetica", 14), width="40", bg=rgb_hack((141, 192, 252)), command=open_spotify)
     spotify.pack(pady=3)
 
-    outlook = Button(root, text="outlook", font=("helvetica", 14), width="40", bg='lightgreen', command=open_outlook)
+    outlook = Button(worked, text="outlook", font=("helvetica", 14), width="40", bg=rgb_hack((141, 192, 252)), command=open_outlook)
     outlook.pack(pady=3)
 
-    word = Button(root, text="word", font=("helvetica", 14), width="40", bg='lightgreen', command=open_word)
+    word = Button(worked, text="word", font=("helvetica", 14), width="40", bg=rgb_hack((141, 192, 252)), command=open_word)
     word.pack(pady=3)
 
-    excell = Button(root, text="excell", font=("helvetica", 14), width="40", bg='lightgreen', command=open_excell)
+    excell = Button(worked, text="excell", font=("helvetica", 14), width="40", bg=rgb_hack((141, 192, 252)), command=open_excell)
     excell.pack(pady=3)
 
-    powerpoint = Button(root, text="powerpoint", font=("helvetica", 14), width="40", bg='lightgreen', command=open_powerpoint)
+    powerpoint = Button(worked, text="powerpoint", font=("helvetica", 14), width="40", bg=rgb_hack((141, 192, 252)), command=open_powerpoint)
     powerpoint.pack(pady=3)
 
-    gmail0 = Button(root, text="gmail 1", font=("helvetica", 14), width="40", bg='lightgreen', command=open_gmail1)
+    gmail0 = Button(worked, text="gmail 1", font=("helvetica", 14), width="40", bg=rgb_hack((141, 192, 252)), command=open_gmail1)
     gmail0.pack(pady=3)
 
-    gmail1 = Button(root, text="gmail 2", font=("helvetica", 14), width="40", bg='lightgreen', command=open_gmail2)
+    gmail1 = Button(worked, text="gmail 2", font=("helvetica", 14), width="40", bg=rgb_hack((141, 192, 252)), command=open_gmail2)
     gmail1.pack(pady=3)
 
-    gmail2 = Button(root, text="gmail 3", font=("helvetica", 14), width="40", bg='lightgreen', command=open_gmail3)
+    gmail2 = Button(worked, text="gmail 3", font=("helvetica", 14), width="40", bg=rgb_hack((141, 192, 252)), command=open_gmail3)
     gmail2.pack(pady=3)
     
-    vm = Button(root, text="VM", font=("helvetica", 14), width="40", bg='lightgreen', command=open_vm)
+    vm = Button(worked, text="VM", font=("helvetica", 14), width="40", bg=rgb_hack((141, 192, 252)), command=open_vm)
     vm.pack(pady=3)
 
-    github = Button(root, text="github", font=("helvetica", 14), width="40", bg='lightgreen' ,command=open_github)
+    github = Button(worked, text="github", font=("helvetica", 14), width="40", bg=rgb_hack((141, 192, 252)) ,command=open_github)
     github.pack(pady=3)
 
-    googledrive = Button(root, text="Google Drive", font=("helvetica", 14), width="40", bg='lightgreen', command=open_drive)
+    googledrive = Button(worked, text="Google Drive", font=("helvetica", 14), width="40", bg=rgb_hack((141, 192, 252)), command=open_drive)
     googledrive.pack(pady=3)
 
-    azn = Button(root, text="Ausbildungsnachweise", font=("helvetica", 14), width="40", bg='lightgreen', command=open_azn)
+    azn = Button(worked, text="Ausbildungsnachweise", font=("helvetica", 14), width="40", bg=rgb_hack((141, 192, 252)), command=open_azn)
     azn.pack(pady=3)
 
 
-
+    bg = PhotoImage(file="wallpaper.png")
+    bg_label = Label(work, image=bg)
+    bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
 
 
 
 
 def privHome():
-    privHome.configure(bg='darkgreen') 
+    privHome.configure(bg=rgb_hack((141, 192, 252))) 
     print("now you are at home")
     #tkinter.messagebox.showinfo('private', 'now you are at home')
     #tkinter.messagebox.showwarning('private', 'now you are at home')
@@ -128,19 +133,20 @@ def privHome():
     root=tkinter.Tk()
     root.title('HOME')  
     root.geometry('500x700')  
-    root.configure(bg='darkgreen')
+    root.configure(bg=rgb_hack((82, 161, 240)))
     #icon for work
     root.iconbitmap("home.ico")
 
     #part for my apps
 
-    whatsapp_web = Button(root, text="web.whatsapp.com", font=("helvetica", 14), width="40", bg='lightgreen', command=open_whatsapp)
+    whatsapp_web = Button(root, text="web.whatsapp.com", font=("helvetica", 14), width="40", bg=rgb_hack((141, 192, 252)), command=open_whatsapp)
     whatsapp_web.pack(pady=3)
-    spotify = Button(root, text="spotify.com", font=("helvetica", 14), width="40", bg='lightgreen', command=open_spotify)
+    spotify = Button(root, text="spotify.com", font=("helvetica", 14), width="40", bg=rgb_hack((141, 192, 252)), command=open_spotify)
     spotify.pack(pady=3)
 
 root = Tk()
 root.geometry('300x100')
+
 frame1 = Frame(root)
 frame2 = Frame(root)
 root.title("where are you")
@@ -148,10 +154,10 @@ root.iconbitmap("start.ico")
 label= Label(frame1,text="choose one",justify=LEFT)
 label.pack(side=LEFT)
 
-work_there = Button(frame2,text="work",command=work)
+work_there = Button(frame2,text="work",width=10, command=work)
 work_there.pack()
 
-privHome = Button(frame2,text="private",command=privHome)
+privHome = Button(frame2,text="private",width=10,command=privHome)
 privHome.pack()
 
 frame1.pack(padx=1,pady=1)
